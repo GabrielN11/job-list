3868ed43bedb022dd0ae1ed713b44d512fad6697
GabrielN11 <gabrielnunes11c@gmail.com>
2022-08-27 15:42:54 -0300

    minor fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 
 
-export default function index() {
+export default function Index() {
     const navigation = useRouter()
 
     useEffect(() => {
